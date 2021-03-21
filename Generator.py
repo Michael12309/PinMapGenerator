e5658bac8f8a00015557b74c6b706e88446967c5
Michael Leonard <michaelleonard5555@gmail.com>
2021-03-21 00:39:53 -0400

    Added loading bar, command line arguments, fixed formatting

--- Generator.py
+++ Generator.py
@@ -1,10 +1,39 @@
+
 from Constants import *
 
-
+image_location = "image.png"
 # size of blocks to round, controls the detail of the final image
 # lower block size = higher quality = more pins
-block_size = 9
-font_size = 8
+block_size = 20
+font_size = 10
+
+weighted_algo = True
+
+if len(sys.argv) <= 4 and len(sys.argv) != 1:
+    # filename is required
+    if not os.path.isfile(sys.argv[1]):
+        print("Cannot find file: " + sys.argv[1])
+        exit()
+    image_location = sys.argv[1]
+
+    # block size is optional
+    if len(sys.argv) == 3:
+        block_size = int(sys.argv[2])
+
+    # algorithm type is optional
+    if len(sys.argv) == 4:
+        if sys.argv[3] == '1':
+            weighted_algo = False
+
+
+else:
+    print('Usage: ', end='')
+    print('python Generator.py image [block size] [use unweighted algorithm?]')
+    exit()
+
+SOURCE_IMAGE = Image.open(image_location)
+SOURCE_IMAGE = SOURCE_IMAGE.convert('RGB')
+
 
 source_width, source_height = SOURCE_IMAGE.size
 demo_image = Image.new('RGB', (source_width, source_height))
@@ -44,7 +73,7 @@ def closest_pin_color(rgb):
     color_diffs = []
     for color in PIN_COLORS:
         cr, cg, cb = color
-        color_diff = sqrt((r - cr)**2 + (g - cg)**2 + (b - cb)**2)
+        color_diff = math.sqrt((r - cr)**2 + (g - cg)**2 + (b - cb)**2)
         color_diffs.append((color_diff, color))
     return min(color_diffs)[1]
 
@@ -53,9 +82,10 @@ def closest_pin_color_weighted(rgb):
     color_diffs = []
     for color in PIN_COLORS:
         cr, cg, cb = color
-        color_diff = sqrt(((r - cr)*0.30)**2 + ((g - cg)*0.59)**2 + ((b - cb)*0.11)**2)
+        color_diff = math.sqrt(((r - cr)*0.30)**2 + ((g - cg)*0.59)**2 + ((b - cb)*0.11)**2)
         color_diffs.append((color_diff, color))
     return min(color_diffs)[1]
+
 
 
 total_pin_count = 0
@@ -64,9 +94,10 @@ all_colors = []
 for source_y in range(0, source_height, block_size):
     for source_x in range(0, source_width, block_size):
         block_rgb = average_rgb_area(source_x, source_y)
-        block_pin_rgb = closest_pin_color(block_rgb)
-        #print(block_rgb)
-        #print(block_pin_rgb)
+        if weighted_algo:
+            block_pin_rgb = closest_pin_color_weighted(block_rgb)
+        else:
+            block_pin_rgb = closest_pin_color(block_rgb)
         fill_area(source_x, source_y, block_pin_rgb)
         demo_pixels[source_x, source_y] = block_rgb
         if block_pin_rgb != (255, 255, 255):
@@ -76,12 +107,48 @@ for source_y in range(0, source_height, block_size):
                              str(PIN_COLORS.index(block_pin_rgb)),
                              font=number_font,
                              fill=(0, 0, 0))
-    print('Loading: {}%'.format(int(source_y/source_height * 100)))
-print('Total pin count: {}'.format(total_pin_count))
+    percent_num = int(source_y/source_height * PROGRESS_WIDTH)
+    percent_text = ' Loading: {:<10}'.format(str(int(source_y/source_height * 100)) + " %")
+
+    # draw the loading bar
+    sys.stdout.write("[ " +
+                     "#"*percent_num +
+                     "-"*int(PROGRESS_WIDTH-percent_num) +
+                     " ]")
+    # write the percentage after loading bar
+    sys.stdout.write(percent_text)
+
+    # move cursor back required amout for the progress bar
+    sys.stdout.write("\b" * (PROGRESS_WIDTH + 4)) # +4 because of chars: "[ " + " ]"
+    # move cursor back required amount for percent text
+    sys.stdout.write("\b"*len(percent_text))
+
+    # send text to screen immediately
+    sys.stdout.flush()
+
+# print out 100% loading bar (sometimes it will end at ~99 or 98%)
+percent_num = PROGRESS_WIDTH
+percent_text = ' Loading: {:<10}'.format("100 %")
+sys.stdout.write("[ " +
+                 "#"*percent_num +
+                 "-"*int(PROGRESS_WIDTH-percent_num) +
+                 " ]")
+sys.stdout.write(percent_text)
+sys.stdout.write("\b" * (PROGRESS_WIDTH + 4))
+sys.stdout.write("\b"*len(percent_text))
+sys.stdout.flush()
+
+print('\n\nTotal pin count: {}\n'.format(total_pin_count))
 all_colors.sort()
 
+# TODO: write the names of each color next to the rgb (maybe use webcolors library)?
+print('{:<10}{:<20}'.format("# Pins", "Color (rgb)"))
+
 for color in PIN_COLORS:
-    print(all_colors.count(color), '   \t', color)
+    print('{:<10}{:<20}'.format(all_colors.count(color), ', '.join([str(num) for num in color])))
 
 demo_image.save('Preview.png', 'PNG')
 number_image.save('Number map.png', 'PNG')
+
+print("\nExample image saved as \"Preview.png\"")
+print("Number map saved as \"Number map.png\"")
